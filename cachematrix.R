9437780fc3e965794883df6565abcf513ebaeb28
Kieran Black <kieran@inkberrycreative.co.uk>
2017-03-13 09:11:22 +0000

    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The below
+# functions cache the inverse of a matrix for repeated use.
 
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse and
+# allows you to:
+# 1. set the value of the vector
+# 2. get the value of the vector
+# 3. set the value of the mean
+# 4. get the value of the mean
 makeCacheMatrix <- function(x = matrix()) {
-
+	stored_inverse <- NULL
+        
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+        
+	get <- function() {
+		x
+	}
+        
+	setinverse <- function(inverse) {
+		stored_inverse <<- inverse
+	}
+        
+	getinverse <- function() {
+		stored_inverse
+	}
+        
+	list(
+		set = set,
+		get = get,
+		setinverse = setinverse,
+		getinverse = getinverse
+	)
 }
 
 
-## Write a short comment describing this function
 
+# This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix.
+
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then we retrieve the inverse from the cache.
+
+# This function assumes that the matrix supplied is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
+
+
+# usage example
+m <- makeCacheMatrix()
+m$set(matrix(c(1,2,3,4), nrow=2, ncol=2, byrow=TRUE))
+cacheSolve(m) # un-cached value
+cacheSolve(m) # cached value
+
